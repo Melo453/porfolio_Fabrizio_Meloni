88dbe7afdb4743df540396fef9059b3601ac7feb
fabri_9800 <51181323+Melo453@users.noreply.github.com>
2022-02-27 20:40:07 -0300

    dark mode y arreglos visuales

--- js/app.js
+++ js/app.js
@@ -68,3 +68,11 @@ $(document).ready(function() {
         );
     })
 })
+
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active-switch');
+});
